7e1033509862f9616f04ccfba6599c2209ca2593
s012456 <s012456@192.168.1.203>
2019-10-31 13:18:29 -0400

    MultiplesOf3And5

--- FCC Algorithms/src/MultiplesOf3and5.java
+++ FCC Algorithms/src/MultiplesOf3and5.java
@@ -1,4 +1,11 @@
-
+import java.awt.Canvas;
 public class MultiplesOf3and5 {
-
+	public static void multiplesOf3and5(int num) {
+		for (var i = 0; i <= Math.floor(num / 3); i++) {
+			System.out.println(i * 3);
+		}
+		for (int i = 0; i <= Math.floor(num / 5); i++) {
+			System.out.println(i * 5);
+		}
+	}
 }
